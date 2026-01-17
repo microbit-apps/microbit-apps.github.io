357ad3f2ad20cac6b795553725af3547c612d8c5
Thomas Ball <thomas.j.ball@outlook.com>
2026-01-16 18:17:59 -0800

    Add MicroData documentation for data collection and analysis tools for micro:bit

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,12 +2,12 @@
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/apps/microbit-robot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapps%2Fmicrobit-robot.mdx&astroContentModuleFlag=true")],
-["src/content/docs/apps/microcode.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapps%2Fmicrocode.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started/setup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fsetup.mdx&astroContentModuleFlag=true")],
+["src/content/docs/apps/microdata.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapps%2Fmicrodata.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting-started/intro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fintro.mdx&astroContentModuleFlag=true")],
-["src/content/docs/apps/display-shield.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapps%2Fdisplay-shield.mdx&astroContentModuleFlag=true")],
+["src/content/docs/apps/microcode.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapps%2Fmicrocode.mdx&astroContentModuleFlag=true")],
+["src/content/docs/extensions/display-shield.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fextensions%2Fdisplay-shield.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting-started/setup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fsetup.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/resources/api.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Fapi.mdx&astroContentModuleFlag=true")],
-["src/content/docs/resources/tutorials.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Ftutorials.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/resources/docs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Fdocs.mdx&astroContentModuleFlag=true")],
-["src/content/docs/extensions/display-shield.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fextensions%2Fdisplay-shield.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/resources/tutorials.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Ftutorials.mdx&astroContentModuleFlag=true")]]);
 		
