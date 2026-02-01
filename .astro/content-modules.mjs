8f294804c4d7cf8d8997e535490ecf373d46f0cf
Thomas Ball <thomas.j.ball@outlook.com>
2026-01-31 22:46:38 -0800

    start on guide

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,14 +1,14 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/microdata/start.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrodata%2Fstart.mdx&astroContentModuleFlag=true")],
-["src/content/docs/microdata/guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrodata%2Fguide.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting-started/display-shields.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fdisplay-shields.mdx&astroContentModuleFlag=true")],
-["src/content/docs/microcode/start.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrocode%2Fstart.mdx&astroContentModuleFlag=true")],
-["src/content/docs/microcode/guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrocode%2Fguide.mdx&astroContentModuleFlag=true")],
-["src/content/docs/resources/display-shield-ui.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Fdisplay-shield-ui.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/extensions/display-shield-ext.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fextensions%2Fdisplay-shield-ext.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting-started/intro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fintro.mdx&astroContentModuleFlag=true")],
+["src/content/docs/microcode/guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrocode%2Fguide.mdx&astroContentModuleFlag=true")],
+["src/content/docs/microcode/start.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrocode%2Fstart.mdx&astroContentModuleFlag=true")],
+["src/content/docs/microdata/start.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrodata%2Fstart.mdx&astroContentModuleFlag=true")],
+["src/content/docs/microdata/guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmicrodata%2Fguide.mdx&astroContentModuleFlag=true")],
+["src/content/docs/resources/display-shield-ui.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Fdisplay-shield-ui.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/resources/faq.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Ffaq.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/resources/troubleshooting.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresources%2Ftroubleshooting.mdx&astroContentModuleFlag=true")]]);
 		
